b3253b3f38b80bdc183107b703b630f424c75098
Dean Erickson <dmepilot@yahoo.com>
2020-06-04 02:36:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,12 +17,10 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.any? do |win_combination|
-   board[win_combination].all? do |pos|
-    if pos == "X" || "O" 
-   #if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" 
+   if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" 
      return win_combination
-    #elsif board[win_combination[0]]== "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-     #return win_combination
+    elsif board[win_combination[0]]== "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+     return win_combination
    else 
      false 
     end

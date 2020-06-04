67b17896ca7f55503735f5e9b4bb39e9b91aa86e
Dean Erickson <dmepilot@yahoo.com>
2020-06-04 02:13:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,8 +20,8 @@ def won?(board)
    if board[win_combination].uniq.length==1 
    #if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" 
      return win_combination
-    elsif board[win_combination[0]]== "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-     return win_combination
+    #elsif board[win_combination[0]]== "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+     #return win_combination
    else 
      false 
     end
